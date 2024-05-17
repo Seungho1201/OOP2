0ecbe317f30729126fea3dd830aacede9644f885
Seungho1201 <ohseungho1201@gmail.com>
2024-05-17 13:45:35 +0900

    test

--- src/object.java
+++ src/object.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class object {
+public class object {
+    public static void main(String[] args) {
+        // 테스트용
+        System.out.println("객체지향 생성자 오버로딩 테스트 코드");
+        student stdObj1 = new student();                // 생성자 호출
+
+        stdObj1.id = 202002056;
+        stdObj1.name = "seungho";
+        stdObj1.printInfo();
+
+        student stdObj2 = new student(202002057);    //일반 생성자 호출
+        // 일반 생성자로 id를 넘겨주어 객체 생성
+        // 즉, id 입력은 인자값으로 대신한 것
+        stdObj2.name = "ohseungho";
+        stdObj2.printInfo();
+
+        student stdObj3 = new student("SSeungho", 202002058);    //일반 생성자 호출(오버로딩)
+        stdObj3.printInfo();
+    }
 }
--- src/student.java
+++ src/student.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class student {
+public class student {
+    int id;
+    String name;
+
+    //생성자는 정의된 클래스와 이름이 같아야 하며 갹체 생성시 자동 호출
+    // 생성자는 어떤 유형도 return X
+    // 메서드는 return이 있읏수도 있고 없을수도 있음
+    // 생성자는 객체 생성시 단 한번만, 메서드는 여러번 호출 가능
+
+
+    // 기본생성자 (매개변수가 없는 생성자)
+    student(){
+        System.out.println("기본 생성자 호출");
+    }
+    // 일반 생성자 (매개변수 있음)
+    student(int param){
+        // 일반 생성자를 호출하며 객체 생성
+        id = param;
+        System.out.println("일반 생성자 호출 id : " + id);
+    }
+    student(String param, int param2){
+        id = param2;
+        name = param;
+        System.out.println("일반 생성자(오버로딩)호출 id : " + id + "이름" + name);
+    }
+
+    void insertRecord(int parm1, String parm2){
+        id = parm1;
+        name = parm2;
+    }
+
+    void printInfo(){
+        System.out.println("아이디 : " + id);
+        System.out.println("이름 : " + name);
+    }
 }
